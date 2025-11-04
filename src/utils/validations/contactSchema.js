302ec407efa3898c852fa747f7808c4419b2370d
mooenz <joss92821@hotmail.com>
2025-11-04 10:21:41 -0500

    feat: Agregar esquema de validación para el formulario de contacto

---
+++ src/utils/validations/contactSchema.js
@@ -0,0 +1,39 @@
+import { z } from 'zod';
+
+export const contactSchema = z.object({
+	nombreContacto: z
+		.string({ required_error: 'Completa este campo' })
+		.trim()
+		.min(2, 'Mínimo 2 caracteres')
+		.max(50, 'Máximo 50 caracteres')
+		.regex(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, 'Ingresa solo letras'),
+
+	apellidoContacto: z
+		.string({ required_error: 'Completa este campo' })
+		.trim()
+		.min(2, 'Mínimo 2 caracteres')
+		.max(50, 'Máximo 50 caracteres')
+		.regex(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, 'Ingresa solo letras'),
+
+	telefonoContacto: z
+		.string({ required_error: 'Completa este campo' })
+		.trim()
+		.min(7, 'Mínimo 7 dígitos')
+		.max(10, 'Máximo 10 dígitos')
+		.regex(/^[0-9]+$/, 'Ingresa solo números'),
+
+	asuntoContacto: z.preprocess(
+		(val) => (val === '' ? undefined : val),
+		z.enum(['informacion-productos', 'soporte-tecnico', 'otros'], {
+			errorMap: () => ({ message: 'Selecciona una opción' }),
+		})
+	),
+
+	emailContacto: z
+		.string({ required_error: 'Ingrese un correo' })
+		.trim()
+		.toLowerCase()
+		.regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Correo con formato inválido'),
+
+	terminosContacto: z.boolean({ required_error: 'Debes aceptar la política de privacidad' }).refine((val) => val === true, 'Debes aceptar la política de privacidad'),
+});

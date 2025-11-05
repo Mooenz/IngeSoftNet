afefe153f747b616c14741b1441cf2c01c5a3e11
mooenz <joss92821@hotmail.com>
2025-11-05 08:54:22 -0500

    feat: Actualizar rutas de menú para eliminar extensiones innecesarias en los enlaces

--- src/constants/menu.js
+++ src/constants/menu.js
@@ -1,28 +1,28 @@
 const menu = {
 	nosotros: {
 		nombre: 'Nosotros',
-		href: '/nosotros.html',
+		href: '/nosotros',
 		icono: 'fi-rr-house-user',
 		submenu: [],
 		esBoton: false,
 	},
 	manual: {
 		nombre: 'Manual de procesos',
-		href: '/manual-procesos.html',
+		href: '/manual-procesos',
 		icono: 'fi-rr-book-bookmark',
 		submenu: [],
 		esBoton: false,
 	},
 	responsabilidad: {
 		nombre: 'Responsabilidad social',
-		href: '/responsabilidad-social.html',
+		href: '/responsabilidad-social',
 		icono: 'fi-rr-badge-check',
 		submenu: [],
 		esBoton: false,
 	},
 	contacto: {
 		nombre: 'Contáctenos',
-		href: '/contacto.html',
+		href: '/contacto',
 		icono: 'fi-rr-mobile-notch',
 		submenu: [],
 		esBoton: true,

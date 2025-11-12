af1d8775566d355e9863ff2237c932e009401cff
mooenz <joss92821@hotmail.com>
2025-11-12 10:24:39 -0500

    feat: Agregar atributos y mejorar estilos en componentes de botón y título fix: Corregir texto y agregar categorías en servicios fix: Actualizar enlaces y mejorar descripciones en secciones

--- src/constants/services.js
+++ src/constants/services.js
@@ -8,21 +8,25 @@ const services = [
 		title: 'Servicio de venta de licencias SysCafé',
 		description: 'Obtén tu licencia oficial SysCafé con asesoría experta y activación confiable para mantener tus procesos contables siempre actualizados y seguros.',
 		image: services1.src || services1,
+		category: 'Licencias y Activación',
 	},
 	{
 		title: 'Soporte y mantenimiento de alto nivel',
 		description: 'Disfruta de un servicio especializado que garantiza el óptimo funcionamiento de tu sistema, con atención rápida y soluciones que previenen interrupciones en tu negocio.',
 		image: services2.src || services2,
+		category: 'Soporte Técnico',
 	},
 	{
 		title: 'Implementación del software SysCafé',
 		description: 'Recibe una instalación profesional con configuración personalizada, asegurando que tu software esté listo para potenciar tus operaciones desde el primer día.',
 		image: services3.src || services3,
+		category: 'Implementación y Configuración',
 	},
 	{
 		title: 'Asesoría contable y tributaria',
 		description: 'Accede a un servicio profesional que te ayuda a cumplir con la normativa fiscal y optimizar la gestión financiera de tu negocio con confianza y respaldo experto.',
 		image: services4.src || services4,
+		category: 'Asesoría Profesional',
 	},
 ];
 

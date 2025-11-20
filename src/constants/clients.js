e42e545205bfb5a18e0a00743907603a0a430bb6
mooenz <joss92821@hotmail.com>
2025-11-19 22:59:41 -0500

    feat: actualizar imágenes de clientes y servicios a formato WebP para mejorar rendimiento

--- src/constants/clients.js
+++ src/constants/clients.js
@@ -1,155 +1,154 @@
 // src/data/clients.js
 
-import logoApDiesel from '@assets/logos/logo-ap-diesel.png';
-import logoBacoLiquors from '@assets/logos/logo-baco-liquors-store.png';
-import logoCablemax from '@assets/logos/logo-cablemax-la-26.png';
-import logoDistrifruver from '@assets/logos/logo-distrifruver-del-llano.png';
-import logoCentro from '@assets/logos/logo-centro-de-diagnóstico-automotor-chocó.png';
-import logoEstanquillo from '@assets/logos/logo-estanquillo-zona-x.png';
-import logoChoquibfruver from '@assets/logos/logo-choquibfruver-andes-ospina.png';
-import logoCascanuez from '@assets/logos/logo-inversiones-cascanuez.png';
-import logoDistribuciones from '@assets/logos/logo-distribuciones-el-lider-quibdó.png';
-import logoMercadiario from '@assets/logos/logo-supermercado-mercadiario.png';
-import logoCombo from '@assets/logos/logo-distribuidora-el-combo.png';
-import logoTambo from '@assets/logos/logo-inversiones-el-tambo.png';
-import logoDMT from '@assets/logos/logo-dmt-soluciones.png';
-import logoFundacion from '@assets/logos/logo-fundación.png';
-import logoKrensi from '@assets/logos/logo-krensi.png';
-import logoFeria from '@assets/logos/logo-la-feria-infantil.png';
-import logoLito from '@assets/logos/logo-lito-legumbrerÍa-y-mercado.png';
-import logoMaquizora from '@assets/logos/logo-maquizora.png';
-import logoPaez from '@assets/logos/logo-paez-martin.png';
-import logoSerpreorientes from '@assets/logos/logo-serpreorientes.png';
-import logoConfirmerka from '@assets/logos/logo-inversiones-confirmerka.png';
+import logoApDiesel from '@assets/logos/logo-ap-diesel.webp';
+import logoBacoLiquors from '@assets/logos/logo-baco-liquors-store.webp';
+import logoCablemax from '@assets/logos/logo-cablemax-la-26.webp';
+import logoDistrifruver from '@assets/logos/logo-distrifruver-del-llano.webp';
+import logoCentro from '@assets/logos/logo-centro-de-diagnóstico-automotor-chocó.webp';
+import logoEstanquillo from '@assets/logos/logo-estanquillo-zona-x.webp';
+import logoChoquibfruver from '@assets/logos/logo-choquibfruver-andes-ospina.webp';
+import logoCascanuez from '@assets/logos/logo-inversiones-cascanuez.webp';
+import logoDistribuciones from '@assets/logos/logo-distribuciones-el-lider-quibdó.webp';
+import logoMercadiario from '@assets/logos/logo-supermercado-mercadiario.webp';
+import logoCombo from '@assets/logos/logo-distribuidora-el-combo.webp';
+import logoTambo from '@assets/logos/logo-inversiones-el-tambo.webp';
+import logoDMT from '@assets/logos/logo-dmt-soluciones.webp';
+import logoFundacion from '@assets/logos/logo-fundación.webp';
+import logoKrensi from '@assets/logos/logo-krensi.webp';
+import logoFeria from '@assets/logos/logo-la-feria-infantil.webp';
+import logoLito from '@assets/logos/logo-lito-legumbrerÍa-y-mercado.webp';
+import logoMaquizora from '@assets/logos/logo-maquizora.webp';
+import logoPaez from '@assets/logos/logo-paez-martin.webp';
+import logoSerpreorientes from '@assets/logos/logo-serpreorientes.webp';
+import logoConfirmerka from '@assets/logos/logo-inversiones-confirmerka.webp';
 
 const clients = [
-  {
-    name: 'Ap Diesel',
-    logo: logoApDiesel,
-    width: 521,
-    height: 61,
-    
-  },
-  {
-    name: 'Baco Liquors Store',
-    logo: logoBacoLiquors,
-    width: 166,
-    height: 116,
-  },
-  {
-    name: 'Cablemax La 26',
-    logo: logoCablemax,
-    width: 264,
-    height: 75,
-  },
-  {
-    name: 'Distrifruver del Llano',
-    logo: logoDistrifruver,
-    width: 320,
-    height: 344,
-  },
-  {
-    name: 'Centro de Diagnóstico Automotor Chocó',
-    logo: logoCentro,
-    width: 551,
-    height: 237,
-  },
-  {
-    name: 'Estanquillo Zona X',
-    logo: logoEstanquillo,
-    width: 500,
-    height: 483,
-  },
-  {
-    name: 'Choquibfruver Andes Ospina',
-    logo: logoChoquibfruver,
-    width: 500,
-    height: 273,
-  },
-  {
-    name: 'Inversiones Cascanuéz',
-    logo: logoCascanuez,
-    width: 360,
-    height: 366,
-  },
-  {
-    name: 'Distribuciones El Líder Quibdó',
-    logo: logoDistribuciones,
-    width: 401,
-    height: 194,
-  },
-  {
-    name: 'Supermercado Mercadiario',
-    logo: logoMercadiario,
-    width: 220,
-    height: 222,
-  },
-  {
-    name: 'Distribuidora El Combo',
-    logo: logoCombo,
-    width: 250,
-    height: 146,
-  },
-  {
-    name: 'Inversiones El Tambo',
-    logo: logoTambo,
-    width: 553,
-    height: 426,
-  },
-  {
-    name: 'DMT Soluciones',
-    logo: logoDMT,
-    width: 176,
-    height: 44,
-  },
-  {
-    name: 'Fundación Vivir Mejor',
-    logo: logoFundacion,
-    width: 320,
-    height: 143,
-  },
-  {
-    name: 'Krensi',
-    logo: logoKrensi,
-    width: 661,
-    height: 137,
-  },
-  {
-    name: 'La Feria Infantil',
-    logo: logoFeria,
-    width: 236,
-    height: 200,
-  },
-  {
-    name: 'Lito Legumbrería y Mercado',
-    logo: logoLito,
-    width: 112,
-    height: 110,
-  },
-  {
-    name: 'Maquizora',
-    logo: logoMaquizora,
-    width: 676,
-    height: 282,
-  },
-  {
-    name: 'Paez Martín',
-    logo: logoPaez,
-    width: 660,
-    height: 126,
-  },
-  {
-    name: 'Serpreorientes',
-    logo: logoSerpreorientes,
-    width: 568,
-    height: 164,
-  },
-  {
-    name: 'Inversiones Confirmerka',
-    logo: logoConfirmerka,
-    width: 572,
-    height: 163,
-  },
+	{
+		name: 'Ap Diesel',
+		logo: logoApDiesel,
+		width: 150,
+		height: 18,
+	},
+	{
+		name: 'Baco Liquors Store',
+		logo: logoBacoLiquors,
+		width: 86,
+		height: 60,
+	},
+	{
+		name: 'Cablemax La 26',
+		logo: logoCablemax,
+		width: 150,
+		height: 43,
+	},
+	{
+		name: 'Distrifruver del Llano',
+		logo: logoDistrifruver,
+		width: 47,
+		height: 50,
+	},
+	{
+		name: 'Centro de Diagnóstico Automotor Chocó',
+		logo: logoCentro,
+		width: 139,
+		height: 60,
+	},
+	{
+		name: 'Estanquillo Zona X',
+		logo: logoEstanquillo,
+		width: 52,
+		height: 50,
+	},
+	{
+		name: 'Choquibfruver Andes Ospina',
+		logo: logoChoquibfruver,
+		width: 110,
+		height: 60,
+	},
+	{
+		name: 'Inversiones Cascanuéz',
+		logo: logoCascanuez,
+		width: 60,
+		height: 60,
+	},
+	{
+		name: 'Distribuciones El Líder Quibdó',
+		logo: logoDistribuciones,
+		width: 124,
+		height: 60,
+	},
+	{
+		name: 'Supermercado Mercadiario',
+		logo: logoMercadiario,
+		width: 59,
+		height: 60,
+	},
+	{
+		name: 'Distribuidora El Combo',
+		logo: logoCombo,
+		width: 103,
+		height: 60,
+	},
+	{
+		name: 'Inversiones El Tambo',
+		logo: logoTambo,
+		width: 78,
+		height: 60,
+	},
+	{
+		name: 'DMT Soluciones',
+		logo: logoDMT,
+		width: 150,
+		height: 38,
+	},
+	{
+		name: 'Fundación Vivir Mejor',
+		logo: logoFundacion,
+		width: 134,
+		height: 60,
+	},
+	{
+		name: 'Krensi',
+		logo: logoKrensi,
+		width: 150,
+		height: 31,
+	},
+	{
+		name: 'La Feria Infantil',
+		logo: logoFeria,
+		width: 71,
+		height: 60,
+	},
+	{
+		name: 'Lito Legumbrería y Mercado',
+		logo: logoLito,
+		width: 61,
+		height: 60,
+	},
+	{
+		name: 'Maquizora',
+		logo: logoMaquizora,
+		width: 144,
+		height: 60,
+	},
+	{
+		name: 'Paez Martín',
+		logo: logoPaez,
+		width: 150,
+		height: 29,
+	},
+	{
+		name: 'Serpreorientes',
+		logo: logoSerpreorientes,
+		width: 150,
+		height: 43,
+	},
+	{
+		name: 'Inversiones Confirmerka',
+		logo: logoConfirmerka,
+		width: 150,
+		height: 43,
+	},
 ];
 
 export default clients;
--- src/constants/services.js
+++ src/constants/services.js
@@ -1,40 +1,56 @@
-import services1 from '@assets/servicesImages/servicio-de-venta-de-licencias-SysCafé.jpg';
-import services2 from '@assets/servicesImages/soporte-y-mantenimiento-de-alto-nivel.jpg';
-import services3 from '@assets/servicesImages/implementación-del-software-SysCafé.jpg';
-import services4 from '@assets/servicesImages/asesoría-contable-y-tributaria.jpg';
+import services1 from '@assets/servicesImages/servicio-de-venta-de-licencias-SysCafé.webp';
+import services1Sm from '@assets/servicesImages/servicio-de-venta-de-licencias-SysCafé-sm.webp';
+import services2 from '@assets/servicesImages/soporte-y-mantenimiento-de-alto-nivel.webp';
+import services2Sm from '@assets/servicesImages/soporte-y-mantenimiento-de-alto-nivel-sm.webp';
+import services3 from '@assets/servicesImages/implementación-del-software-SysCafé.webp';
+import services3Sm from '@assets/servicesImages/implementación-del-software-SysCafé-sm.webp';
+import services4 from '@assets/servicesImages/asesoría-contable-y-tributaria.webp';
+import services4Sm from '@assets/servicesImages/asesoría-contable-y-tributaria-sm.webp';
 
 const services = [
 	{
 		title: 'Servicio de venta de licencias SysCafé',
 		description: 'Obtén tu licencia oficial SysCafé con asesoría experta y activación confiable para mantener tus procesos contables siempre actualizados y seguros.',
-		image: services1.src || services1,
+		image: services1,
+		imageSm: services1Sm,
 		category: 'Licencias y Activación',
-		width: 1500,
-		height: 1071,
+		width: 1000,
+		widthSm: 500,
+		height: 1000,
+		heightSm: 500,
 	},
 	{
 		title: 'Soporte y mantenimiento de alto nivel',
 		description: 'Disfruta de un servicio especializado que garantiza el óptimo funcionamiento de tu sistema, con atención rápida y soluciones que previenen interrupciones en tu negocio.',
-		image: services2.src || services2,
+		image: services2,
+		imageSm: services2Sm,
 		category: 'Soporte Técnico',
-		width: 1500,
+		width: 1000,
+		widthSm: 500,
 		height: 1000,
+		heightSm: 500,
 	},
 	{
 		title: 'Implementación del software SysCafé',
 		description: 'Recibe una instalación profesional con configuración personalizada, asegurando que tu software esté listo para potenciar tus operaciones desde el primer día.',
-		image: services3.src || services3,
+		image: services3,
+		imageSm: services3Sm,
 		category: 'Implementación y Configuración',
-		width: 1500,
-		height: 1001,
+		width: 1000,
+		widthSm: 500,
+		height: 1000,
+		heightSm: 500,
 	},
 	{
 		title: 'Asesoría contable y tributaria',
 		description: 'Accede a un servicio profesional que te ayuda a cumplir con la normativa fiscal y optimizar la gestión financiera de tu negocio con confianza y respaldo experto.',
-		image: services4.src || services4,
+		image: services4,
+		imageSm: services4Sm,
 		category: 'Asesoría Profesional',
-		width: 1500,
-		height: 1001,
+		width: 1000,
+		widthSm: 500,
+		height: 1000,
+		heightSm: 500,
 	},
 ];
 
